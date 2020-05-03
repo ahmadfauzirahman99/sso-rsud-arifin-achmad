edaa7736f5aa9de1dcdf909cccd8312b18977964
Ahmad Fauzi Rahman <ahmadfauzirahman99@gmail.com>
2020-05-03 16:44:49 +0700

    Penambahan Alias Untuk Aplikasi RSUD

---
+++ config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+Yii::setAlias('@.sso', 'http://sso.rsud-arifin.localhost');
+Yii::setAlias('@.inap', 'http://inap.rsud-arifin.localhost');
+Yii::setAlias('@.jalan', 'http://jalan.rsud-arifin.localhost');
+Yii::setAlias('@.akuntansi', 'http://akuntansi.rsud-arifin.localhost');
+Yii::setAlias('@.farmasi', 'http://farmasi.rsud-arifin.localhost');
+Yii::setAlias('@.pegawai', 'http://pegawai.rsud-arifin.localhost');
+Yii::setAlias('@.dashboard', 'http://dashboard.rsud-arifin.localhost');
+Yii::setAlias('@.gizi', 'http://gizi.rsud-arifin.localhost');
+Yii::setAlias('@.pendaftaran', 'http://pendaftaran.rsud-arifin.localhost');
