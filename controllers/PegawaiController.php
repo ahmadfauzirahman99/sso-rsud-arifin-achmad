1f3299cfd0b3e66abe2747e56ac5efe1d2582079
Ahmad Fauzi Rahman <ahmadfauzirahman99@gmail.com>
2020-05-03 15:04:56 +0700

    Penambahan Filter PegawaiController.php

--- controllers/PegawaiController.php
+++ controllers/PegawaiController.php
@@ -7,6 +7,7 @@ use app\components\HelperSso;
 use Yii;
 use app\models\TbPegawai;
 use app\models\TbPegawaiSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -17,17 +18,34 @@ use yii\filters\VerbFilter;
 class PegawaiController extends Controller
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function behaviors()
     {
-        $this->layout = Auth::getRole();
-        return Auth::behaviors([
-            'deny' => function ($rule, $action) {
-                $this->redirect(['masuk/index']);
-            },
-        ]);
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
     }
+
 
     /**
      * Lists all TbPegawai models.
